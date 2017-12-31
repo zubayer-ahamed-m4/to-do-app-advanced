0846853181c15207d51360d7ec91366fa0af8241
zubayer-ahamed-m4 <zubayer.ahamed@metafour.com>
2017-12-31 07:06:01 +0600

    first time security emplimentation

---
+++ src/main/java/com/coderslab/security/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.coderslab.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author cyclingbd007
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/todo", "/todo/**").authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+	}
+	
+	@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("zubayer").password("1234").roles("USER");
+    }
+
+}
